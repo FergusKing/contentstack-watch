ff26dcd9bea33274113733ad4a14f6f70894a154
Fergus King <fergus.king@sse.com>
2018-07-26 22:01:25 +0100

    Server updates

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
+const http = require('http')
+const debug = require('debug')('contentstack-watch:server')
 const express = require('express')
 const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+
+
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
+var server = http.createServer(app)
+const io = require('socket.io')(server)
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+function normalizePort (val) {
+    var port = parseInt(val, 10)
+    if (isNaN(port)) {
+      return val
+    }
+    if (port >= 0) {
+      return port
+    }
+    return false
+}
+function onError (error) {
+    if (error.syscall !== 'listen') {
+        throw error
+    }
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port
+    switch (error.code) {
+        case 'EACCES':
+        console.error(bind + ' requires elevated privileges')
+        process.exit(1)
+        break
+        case 'EADDRINUSE':
+        console.error(bind + ' is already in use')
+        process.exit(1)
+        break
+        default:
+        throw error
+    }
+}
+function onListening () {
+    var addr = server.address()
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port
+    debug('Listening on ' + bind)
+}
+
 
 app.use(express.static('webroot'))
 
@@ -32,14 +81,6 @@ io.on('connection', function(socket){
   })
 
 });
-
-http.listen(3000, function(err){
-  if(err){
-    console.log(err)
-  }else{
-    console.log('listening on *:3000');
-  }
-})
 
 function sendMessage(msg){
   console.log(clients)
